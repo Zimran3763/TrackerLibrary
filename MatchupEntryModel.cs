b79b7a0234177fd1ae3999c2e777dff2a55367a0
imran zaman <imran_ujhani@yahoo.com>
2020-02-16 13:45:00 -0500

    commit  tournament tracker library

---
+++ MatchupEntryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCometing { get; set; }
+        public double Score { set; get; }
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
---
+++ MatchupModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupEntryMOdel> Entries { get; set; } = new List<MatchupEntryMOdel>();
+        public TeamModel Winner { get; set; }
+        public int MacthupRound { get; set; }
+    }
+}
